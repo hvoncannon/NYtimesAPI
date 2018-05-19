c72e3accfe2565f13362d7c9dbca51c43cf91cbd
hvoncannon <hayden.voncannon@gmail.com>
2018-05-19 13:35:19 -0700

    simplified call url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,12 @@
 $("#run-search").on("click", function () {
     event.preventDefault();
     var userQ = $("#search-term").val();
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + '?'
-        + $.param({
-            'api-key': "78b7adbaef6b44d2b859569c1ff4814e",
-            'q': userQ,
-            'begin_date' : "",
-            "end_date" : ""
-        });
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + '?' + "api-key=78b7adbaef6b44d2b859569c1ff4814e&q=" + userQ
     $.ajax({
         url: queryURL,
         method: 'GET',
     }).then(function(response){
         console.log(response);
+        var newDiv = $("<div>")
     })
 })

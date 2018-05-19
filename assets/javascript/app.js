daf54ba393d827585690291e4e64ab2cdb09535b
hvoncannon <hayden.voncannon@gmail.com>
2018-05-19 13:54:56 -0700

    added loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,25 @@
 $("#run-search").on("click", function () {
     event.preventDefault();
     var userQ = $("#search-term").val().trim();
+    var start = $('#start-year').val().trim();
+    var end = $('#end-year').val().trim();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + '?' + "api-key=78b7adbaef6b44d2b859569c1ff4814e&q=" + userQ
+    if (start !== "") {
+        queryURL += '&begin_date=' + start + '0101';
+    }
+    if (end !== "") {
+        queryURL += '&end_date=' + end + '1231';
+    }
     $.ajax({
         url: queryURL,
         method: 'GET',
-    }).then(function(response){
-        console.log(response);
-        console.log(response.response.docs[0].byline);
+    }).then(function (response) {
+        for (var i = 0; i < 10; i++) {
+        console.log(response[i]);
+        console.log(response.response.docs[i].byline);
         var newDiv = $("<div>")
         $("#article-section").append(response.response.docs[0].headline.main);
+        $("#article-section").append("<div>" + response.response.docs[0].byline.original + "</div>");
+    }
     })
 })

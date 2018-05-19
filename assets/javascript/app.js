dfcec70f79f769696cc5699db2fb24a04e346bbe
hvoncannon <hayden.voncannon@gmail.com>
2018-05-19 13:45:53 -0700

    appending headline

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
 $("#run-search").on("click", function () {
     event.preventDefault();
-    var userQ = $("#search-term").val();
+    var userQ = $("#search-term").val().trim();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + '?' + "api-key=78b7adbaef6b44d2b859569c1ff4814e&q=" + userQ
     $.ajax({
         url: queryURL,
         method: 'GET',
     }).then(function(response){
         console.log(response);
+        console.log(response.response.docs[0].byline);
         var newDiv = $("<div>")
+        $("#article-section").append(response.response.docs[0].headline.main);
     })
 })
